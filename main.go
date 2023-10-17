592a00036c625fad494bef5451f8112740a0b519
henrry <henrrybrgt@gmail.com>
2023-10-16 22:57:32 -0400

    adding daisyui

--- main.go
+++ main.go
@@ -37,10 +37,11 @@ func Page(title, path string, body g.Node) g.Node {
 		Title:    title,
 		Language: "en",
 		Head: []g.Node{
+			h.Link(h.Rel("stylesheet"), h.Type("text/css"), h.Href("https://cdn.jsdelivr.net/npm/daisyui@3.9.2/dist/full.css")),
 			h.Script(h.Src("https://cdn.tailwindcss.com?plugins=typography")),
 		},
 		Body: []g.Node{
-			Navbar(path, links), Container(Prose(body), PageFooter()),
+			NavbarDaisy(links), Container(Prose(body), PageFooter()),
 		},
 	})
 }
@@ -53,13 +54,17 @@ type PageLink struct {
 func Navbar(currentPath string, links []PageLink) g.Node {
 	return h.Nav(h.Class("bg-gray-700 mb-4"),
 		Container(
-			h.Div(h.Class("flex items-center space-x-4 h-16"),
-				NavbarLink("/", "Home", currentPath == "/"),
-
-				// We can Map custom slices to Nodes
-				g.Group(g.Map(links, func(l PageLink) g.Node {
-					return NavbarLink(l.Path, l.Name, currentPath == l.Path)
-				})),
+			h.Div(h.Class("flex justify-between items-center space-x-4 h-16"),
+				h.H1(
+					h.Class("text-3xl font-semibold"),
+					h.A(h.Href("/"), g.Text("Bourgomponents")),
+				),
+				h.Div(
+					h.Class("flex items-center space-x-4"),
+					g.Group(g.Map(links, func(l PageLink) g.Node {
+						return NavbarLink(l.Path, l.Name, currentPath == l.Path)
+					})),
+				),
 			),
 		),
 	)
@@ -78,11 +83,11 @@ func NavbarLink(path, text string, active bool) g.Node {
 }
 
 func Container(children ...g.Node) g.Node {
-	return h.Div(h.Class("max-w-7xl mx-auto px-2 sm:px-6 lg:px-8"), g.Group(children))
+	return h.Main(h.Class("max-w-7xl mx-auto px-2 sm:px-6 lg:px-8"), g.Group(children))
 }
 
 func Prose(children ...g.Node) g.Node {
-	return h.Div(h.Class("prose"), g.Group(children))
+	return h.Section(h.Class("prose"), g.Group(children))
 }
 
 func PageFooter() g.Node {
@@ -97,26 +102,5 @@ func PageFooter() g.Node {
 		),
 
 		h.P(h.A(h.Href("https://www.gomponents.com"), g.Text("gomponents"))),
-	)
-}
-
-func indexPage() (string, g.Node) {
-	return "Welcome!", h.Div(
-		h.H1(g.Text("Welcome to this example page")),
-		h.P(g.Text("I hope it will make you happy. 😄 It's using TailwindCSS for styling.")),
-	)
-}
-
-func contactPage() (string, g.Node) {
-	return "Contact", h.Div(
-		h.H1(g.Text("Contact us")),
-		h.P(g.Text("Just do it.")),
-	)
-}
-
-func aboutPage() (string, g.Node) {
-	return "About", h.Div(
-		h.H1(g.Text("About this site")),
-		h.P(g.Text("This is a site showing off gomponents.")),
 	)
 }
